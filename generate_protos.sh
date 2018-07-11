a788cdfc680857b6000d95fa49f138cc947470e7
Johnny Burns <jonathan.burns522@gmail.com>
2018-07-10 17:04:02 -0700

    update for python (#7)

--- generate_protos.sh
+++ generate_protos.sh
@@ -3,5 +3,5 @@ rm -rf $DIR/gen
 docker run -v $DIR:/defs namely/protoc-all:1.11 -d example --with-gateway -l go
 docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/admin --with-gateway -l go
 docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/core --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/admin --with-gateway -l python
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/core --with-gateway -l python
+docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/admin -l python
+docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/core -l python
