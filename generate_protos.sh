20df73f22f6b8b5390f94ec1789817cfd00627c7
Johnny Burns <jonathan.burns522@gmail.com>
2018-07-17 14:46:06 -0700

    moving the dir (#13)

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,8 +1,8 @@
 DIR=`pwd`
-rm -rf $DIR/gen
-docker run -v $DIR:/defs namely/protoc-all:1.11 -d example --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d service --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/admin --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/core --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/admin -l python
-docker run -v $DIR:/defs namely/protoc-all:1.11 -i ./protos -d protos/core -l python
+rm -rf $DIR/flyteidl
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb-go -d example --with-gateway -l go
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb-go -i ./protos -d service --with-gateway -l go
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb-go -i ./protos -d protos/admin --with-gateway -l go
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb-go -i ./protos -d protos/core --with-gateway -l go
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb_python -i ./protos -d protos/admin -l python
+docker run -v $DIR:/defs namely/protoc-all:1.11 -o flyteidl/pb_python -i ./protos -d protos/core -l python
