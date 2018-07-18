3611d7e46ac125e52f1f0aba5db0253dc10e9508
Johnny Burns <jonathan.burns522@gmail.com>
2018-07-18 14:54:15 -0700

    moving service proto (#17)

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,7 +1,7 @@
 DIR=`pwd`
 rm -rf $DIR/gen
 docker run -v $DIR:/defs namely/protoc-all:1.11  -d example --with-gateway -l go
-docker run -v $DIR:/defs namely/protoc-all:1.11  -i ./protos -d service --with-gateway -l go
+docker run -v $DIR:/defs namely/protoc-all:1.11  -i ./protos -d protos/flyteidl/service --with-gateway -l go
 docker run -v $DIR:/defs namely/protoc-all:1.11  -i ./protos -d protos/flyteidl/admin --with-gateway -l go
 docker run -v $DIR:/defs namely/protoc-all:1.11  -i ./protos -d protos/flyteidl/core --with-gateway -l go
 docker run -v $DIR:/defs namely/protoc-all:1.11  -i ./protos -d protos/flyteidl/admin -l python
