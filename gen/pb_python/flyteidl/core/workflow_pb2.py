32071886e4a12ab8a456619cc5fc40ecaa9a2dc8
Haytham AbuelFutuh <haytham@afutuh.com>
2018-08-07 08:52:23 -0700

    add connections (#38)

--- gen/pb_python/flyteidl/core/workflow_pb2.py
+++ gen/pb_python/flyteidl/core/workflow_pb2.py
@@ -24,7 +24,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='flyteidl/core/workflow.proto',
   package='flyteidl.core',
   syntax='proto3',
-  serialized_pb=_b('\n\x1c\x66lyteidl/core/workflow.proto\x12\rflyteidl.core\x1a\x1d\x66lyteidl/core/interface.proto\x1a\x1c\x66lyteidl/core/literals.proto\x1a\x19\x66lyteidl/core/types.proto\x1a\x1d\x66lyteidl/core/condition.proto\x1a\x1egoogle/protobuf/duration.proto\"f\n\x07IfBlock\x12\x33\n\tcondition\x18\x01 \x01(\x0b\x32 .flyteidl.core.BooleanExpression\x12&\n\tthen_node\x18\x02 \x01(\x0b\x32\x13.flyteidl.core.Node\"\xb6\x01\n\x0bIfElseBlock\x12$\n\x04\x63\x61se\x18\x01 \x01(\x0b\x32\x16.flyteidl.core.IfBlock\x12%\n\x05other\x18\x02 \x03(\x0b\x32\x16.flyteidl.core.IfBlock\x12(\n\telse_node\x18\x03 \x01(\x0b\x32\x13.flyteidl.core.NodeH\x00\x12%\n\x05\x65rror\x18\x04 \x01(\x0b\x32\x14.flyteidl.core.ErrorH\x00\x42\t\n\x07\x64\x65\x66\x61ult\"9\n\nBranchNode\x12+\n\x07if_else\x18\x02 \x01(\x0b\x32\x1a.flyteidl.core.IfElseBlock\"/\n\x08TaskNode\x12\x16\n\x0creference_id\x18\x01 \x01(\tH\x00\x42\x0b\n\treference\"}\n\x0cWorkflowNode\x12\"\n\x18launch_plan_reference_id\x18\x01 \x01(\tH\x00\x12<\n\x11workflow_template\x18\x02 \x01(\x0b\x32\x1f.flyteidl.core.WorkflowTemplateH\x00\x42\x0b\n\treference\"w\n\x0cNodeMetadata\x12\x0c\n\x04name\x18\x01 \x01(\t\x12*\n\x07timeout\x18\x04 \x01(\x0b\x32\x19.google.protobuf.Duration\x12-\n\x07retries\x18\x05 \x01(\x0b\x32\x1c.flyteidl.core.RetryStrategy\"#\n\x05\x41lias\x12\x0b\n\x03var\x18\x01 \x01(\t\x12\r\n\x05\x61lias\x18\x02 \x01(\t\"\xd2\x02\n\x04Node\x12\n\n\x02id\x18\x01 \x01(\t\x12-\n\x08metadata\x18\x02 \x01(\x0b\x32\x1b.flyteidl.core.NodeMetadata\x12&\n\x06inputs\x18\x03 \x03(\x0b\x32\x16.flyteidl.core.Binding\x12\x19\n\x11upstream_node_ids\x18\x04 \x03(\t\x12,\n\x0eoutput_aliases\x18\x05 \x03(\x0b\x32\x14.flyteidl.core.Alias\x12,\n\ttask_node\x18\x06 \x01(\x0b\x32\x17.flyteidl.core.TaskNodeH\x00\x12\x34\n\rworkflow_node\x18\x07 \x01(\x0b\x32\x1b.flyteidl.core.WorkflowNodeH\x00\x12\x30\n\x0b\x62ranch_node\x18\x08 \x01(\x0b\x32\x19.flyteidl.core.BranchNodeH\x00\x42\x08\n\x06target\"*\n\x10WorkflowMetadata\x12\x16\n\x0e\x65xecution_role\x18\x01 \x01(\t\"\xfb\x01\n\x10WorkflowTemplate\x12\n\n\x02id\x18\x01 \x01(\t\x12\x31\n\x08metadata\x18\x02 \x01(\x0b\x32\x1f.flyteidl.core.WorkflowMetadata\x12\x30\n\tinterface\x18\x03 \x01(\x0b\x32\x1d.flyteidl.core.TypedInterface\x12\"\n\x05nodes\x18\x04 \x03(\x0b\x32\x13.flyteidl.core.Node\x12\'\n\x07outputs\x18\x05 \x03(\x0b\x32\x16.flyteidl.core.Binding\x12)\n\x0c\x66\x61ilure_node\x18\x06 \x01(\x0b\x32\x13.flyteidl.core.NodeB2Z0github.com/lyft/flyteidl/gen/pb-go/flyteidl/coreb\x06proto3')
+  serialized_pb=_b('\n\x1c\x66lyteidl/core/workflow.proto\x12\rflyteidl.core\x1a\x1d\x66lyteidl/core/interface.proto\x1a\x1c\x66lyteidl/core/literals.proto\x1a\x19\x66lyteidl/core/types.proto\x1a\x1d\x66lyteidl/core/condition.proto\x1a\x1egoogle/protobuf/duration.proto\"f\n\x07IfBlock\x12\x33\n\tcondition\x18\x01 \x01(\x0b\x32 .flyteidl.core.BooleanExpression\x12&\n\tthen_node\x18\x02 \x01(\x0b\x32\x13.flyteidl.core.Node\"\xb6\x01\n\x0bIfElseBlock\x12$\n\x04\x63\x61se\x18\x01 \x01(\x0b\x32\x16.flyteidl.core.IfBlock\x12%\n\x05other\x18\x02 \x03(\x0b\x32\x16.flyteidl.core.IfBlock\x12(\n\telse_node\x18\x03 \x01(\x0b\x32\x13.flyteidl.core.NodeH\x00\x12%\n\x05\x65rror\x18\x04 \x01(\x0b\x32\x14.flyteidl.core.ErrorH\x00\x42\t\n\x07\x64\x65\x66\x61ult\"9\n\nBranchNode\x12+\n\x07if_else\x18\x02 \x01(\x0b\x32\x1a.flyteidl.core.IfElseBlock\"/\n\x08TaskNode\x12\x16\n\x0creference_id\x18\x01 \x01(\tH\x00\x42\x0b\n\treference\"}\n\x0cWorkflowNode\x12\"\n\x18launch_plan_reference_id\x18\x01 \x01(\tH\x00\x12<\n\x11workflow_template\x18\x02 \x01(\x0b\x32\x1f.flyteidl.core.WorkflowTemplateH\x00\x42\x0b\n\treference\"w\n\x0cNodeMetadata\x12\x0c\n\x04name\x18\x01 \x01(\t\x12*\n\x07timeout\x18\x04 \x01(\x0b\x32\x19.google.protobuf.Duration\x12-\n\x07retries\x18\x05 \x01(\x0b\x32\x1c.flyteidl.core.RetryStrategy\"#\n\x05\x41lias\x12\x0b\n\x03var\x18\x01 \x01(\t\x12\r\n\x05\x61lias\x18\x02 \x01(\t\"\xd2\x02\n\x04Node\x12\n\n\x02id\x18\x01 \x01(\t\x12-\n\x08metadata\x18\x02 \x01(\x0b\x32\x1b.flyteidl.core.NodeMetadata\x12&\n\x06inputs\x18\x03 \x03(\x0b\x32\x16.flyteidl.core.Binding\x12\x19\n\x11upstream_node_ids\x18\x04 \x03(\t\x12,\n\x0eoutput_aliases\x18\x05 \x03(\x0b\x32\x14.flyteidl.core.Alias\x12,\n\ttask_node\x18\x06 \x01(\x0b\x32\x17.flyteidl.core.TaskNodeH\x00\x12\x34\n\rworkflow_node\x18\x07 \x01(\x0b\x32\x1b.flyteidl.core.WorkflowNodeH\x00\x12\x30\n\x0b\x62ranch_node\x18\x08 \x01(\x0b\x32\x19.flyteidl.core.BranchNodeH\x00\x42\x08\n\x06target\"*\n\x10WorkflowMetadata\x12\x16\n\x0e\x65xecution_role\x18\x01 \x01(\t\"\xae\x02\n\x10WorkflowTemplate\x12\n\n\x02id\x18\x01 \x01(\t\x12\x31\n\x08metadata\x18\x02 \x01(\x0b\x32\x1f.flyteidl.core.WorkflowMetadata\x12\x30\n\tinterface\x18\x03 \x01(\x0b\x32\x1d.flyteidl.core.TypedInterface\x12\"\n\x05nodes\x18\x04 \x03(\x0b\x32\x13.flyteidl.core.Node\x12\'\n\x07outputs\x18\x05 \x03(\x0b\x32\x16.flyteidl.core.Binding\x12)\n\x0c\x66\x61ilure_node\x18\x06 \x01(\x0b\x32\x13.flyteidl.core.Node\x12\x31\n\x0b\x63onnections\x18\x07 \x01(\x0b\x32\x1c.flyteidl.core.ConnectionSet\"\xd4\x02\n\rConnectionSet\x12@\n\ndownstream\x18\x07 \x03(\x0b\x32,.flyteidl.core.ConnectionSet.DownstreamEntry\x12<\n\x08upstream\x18\x08 \x03(\x0b\x32*.flyteidl.core.ConnectionSet.UpstreamEntry\x1a\x15\n\x06IdList\x12\x0b\n\x03ids\x18\x01 \x03(\t\x1aV\n\x0f\x44ownstreamEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x32\n\x05value\x18\x02 \x01(\x0b\x32#.flyteidl.core.ConnectionSet.IdList:\x02\x38\x01\x1aT\n\rUpstreamEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x32\n\x05value\x18\x02 \x01(\x0b\x32#.flyteidl.core.ConnectionSet.IdList:\x02\x38\x01\x42\x32Z0github.com/lyft/flyteidl/gen/pb-go/flyteidl/coreb\x06proto3')
   ,
   dependencies=[flyteidl_dot_core_dot_interface__pb2.DESCRIPTOR,flyteidl_dot_core_dot_literals__pb2.DESCRIPTOR,flyteidl_dot_core_dot_types__pb2.DESCRIPTOR,flyteidl_dot_core_dot_condition__pb2.DESCRIPTOR,google_dot_protobuf_dot_duration__pb2.DESCRIPTOR,])
 
@@ -476,6 +476,13 @@ _WORKFLOWTEMPLATE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='connections', full_name='flyteidl.core.WorkflowTemplate.connections', index=6,
+      number=7, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -489,7 +496,149 @@ _WORKFLOWTEMPLATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=1266,
-  serialized_end=1517,
+  serialized_end=1568,
+)
+
+
+_CONNECTIONSET_IDLIST = _descriptor.Descriptor(
+  name='IdList',
+  full_name='flyteidl.core.ConnectionSet.IdList',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='ids', full_name='flyteidl.core.ConnectionSet.IdList.ids', index=0,
+      number=1, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1716,
+  serialized_end=1737,
+)
+
+_CONNECTIONSET_DOWNSTREAMENTRY = _descriptor.Descriptor(
+  name='DownstreamEntry',
+  full_name='flyteidl.core.ConnectionSet.DownstreamEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='key', full_name='flyteidl.core.ConnectionSet.DownstreamEntry.key', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='value', full_name='flyteidl.core.ConnectionSet.DownstreamEntry.value', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=_descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001')),
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1739,
+  serialized_end=1825,
+)
+
+_CONNECTIONSET_UPSTREAMENTRY = _descriptor.Descriptor(
+  name='UpstreamEntry',
+  full_name='flyteidl.core.ConnectionSet.UpstreamEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='key', full_name='flyteidl.core.ConnectionSet.UpstreamEntry.key', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='value', full_name='flyteidl.core.ConnectionSet.UpstreamEntry.value', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=_descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001')),
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1827,
+  serialized_end=1911,
+)
+
+_CONNECTIONSET = _descriptor.Descriptor(
+  name='ConnectionSet',
+  full_name='flyteidl.core.ConnectionSet',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='downstream', full_name='flyteidl.core.ConnectionSet.downstream', index=0,
+      number=7, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='upstream', full_name='flyteidl.core.ConnectionSet.upstream', index=1,
+      number=8, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[_CONNECTIONSET_IDLIST, _CONNECTIONSET_DOWNSTREAMENTRY, _CONNECTIONSET_UPSTREAMENTRY, ],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1571,
+  serialized_end=1911,
 )
 
 _IFBLOCK.fields_by_name['condition'].message_type = flyteidl_dot_core_dot_condition__pb2._BOOLEANEXPRESSION
@@ -537,6 +686,14 @@ _WORKFLOWTEMPLATE.fields_by_name['interface'].message_type = flyteidl_dot_core_d
 _WORKFLOWTEMPLATE.fields_by_name['nodes'].message_type = _NODE
 _WORKFLOWTEMPLATE.fields_by_name['outputs'].message_type = flyteidl_dot_core_dot_literals__pb2._BINDING
 _WORKFLOWTEMPLATE.fields_by_name['failure_node'].message_type = _NODE
+_WORKFLOWTEMPLATE.fields_by_name['connections'].message_type = _CONNECTIONSET
+_CONNECTIONSET_IDLIST.containing_type = _CONNECTIONSET
+_CONNECTIONSET_DOWNSTREAMENTRY.fields_by_name['value'].message_type = _CONNECTIONSET_IDLIST
+_CONNECTIONSET_DOWNSTREAMENTRY.containing_type = _CONNECTIONSET
+_CONNECTIONSET_UPSTREAMENTRY.fields_by_name['value'].message_type = _CONNECTIONSET_IDLIST
+_CONNECTIONSET_UPSTREAMENTRY.containing_type = _CONNECTIONSET
+_CONNECTIONSET.fields_by_name['downstream'].message_type = _CONNECTIONSET_DOWNSTREAMENTRY
+_CONNECTIONSET.fields_by_name['upstream'].message_type = _CONNECTIONSET_UPSTREAMENTRY
 DESCRIPTOR.message_types_by_name['IfBlock'] = _IFBLOCK
 DESCRIPTOR.message_types_by_name['IfElseBlock'] = _IFELSEBLOCK
 DESCRIPTOR.message_types_by_name['BranchNode'] = _BRANCHNODE
@@ -547,6 +704,7 @@ DESCRIPTOR.message_types_by_name['Alias'] = _ALIAS
 DESCRIPTOR.message_types_by_name['Node'] = _NODE
 DESCRIPTOR.message_types_by_name['WorkflowMetadata'] = _WORKFLOWMETADATA
 DESCRIPTOR.message_types_by_name['WorkflowTemplate'] = _WORKFLOWTEMPLATE
+DESCRIPTOR.message_types_by_name['ConnectionSet'] = _CONNECTIONSET
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 IfBlock = _reflection.GeneratedProtocolMessageType('IfBlock', (_message.Message,), dict(
@@ -619,7 +777,42 @@ WorkflowTemplate = _reflection.GeneratedProtocolMessageType('WorkflowTemplate', 
   ))
 _sym_db.RegisterMessage(WorkflowTemplate)
 
+ConnectionSet = _reflection.GeneratedProtocolMessageType('ConnectionSet', (_message.Message,), dict(
+
+  IdList = _reflection.GeneratedProtocolMessageType('IdList', (_message.Message,), dict(
+    DESCRIPTOR = _CONNECTIONSET_IDLIST,
+    __module__ = 'flyteidl.core.workflow_pb2'
+    # @@protoc_insertion_point(class_scope:flyteidl.core.ConnectionSet.IdList)
+    ))
+  ,
+
+  DownstreamEntry = _reflection.GeneratedProtocolMessageType('DownstreamEntry', (_message.Message,), dict(
+    DESCRIPTOR = _CONNECTIONSET_DOWNSTREAMENTRY,
+    __module__ = 'flyteidl.core.workflow_pb2'
+    # @@protoc_insertion_point(class_scope:flyteidl.core.ConnectionSet.DownstreamEntry)
+    ))
+  ,
+
+  UpstreamEntry = _reflection.GeneratedProtocolMessageType('UpstreamEntry', (_message.Message,), dict(
+    DESCRIPTOR = _CONNECTIONSET_UPSTREAMENTRY,
+    __module__ = 'flyteidl.core.workflow_pb2'
+    # @@protoc_insertion_point(class_scope:flyteidl.core.ConnectionSet.UpstreamEntry)
+    ))
+  ,
+  DESCRIPTOR = _CONNECTIONSET,
+  __module__ = 'flyteidl.core.workflow_pb2'
+  # @@protoc_insertion_point(class_scope:flyteidl.core.ConnectionSet)
+  ))
+_sym_db.RegisterMessage(ConnectionSet)
+_sym_db.RegisterMessage(ConnectionSet.IdList)
+_sym_db.RegisterMessage(ConnectionSet.DownstreamEntry)
+_sym_db.RegisterMessage(ConnectionSet.UpstreamEntry)
+
 
 DESCRIPTOR.has_options = True
 DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('Z0github.com/lyft/flyteidl/gen/pb-go/flyteidl/core'))
+_CONNECTIONSET_DOWNSTREAMENTRY.has_options = True
+_CONNECTIONSET_DOWNSTREAMENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
+_CONNECTIONSET_UPSTREAMENTRY.has_options = True
+_CONNECTIONSET_UPSTREAMENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
 # @@protoc_insertion_point(module_scope)
