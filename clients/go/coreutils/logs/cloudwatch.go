069807744df2c66c06f5ae06b446d28d5cbb57cf
matthewphsmith <matthewsmith@lyft.com>
2019-01-03 12:16:44 -0800

    Fix Cloudwatch Log Plugin (#130)
    1.  We need to trim prefixes like "docker://" and "cri-o://".
    2.  The group name needs to be configured.

--- clients/go/coreutils/logs/cloudwatch.go
+++ clients/go/coreutils/logs/cloudwatch.go
@@ -3,17 +3,29 @@ package logs
 import (
 	"fmt"
 	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	"strings"
 )
 
 type cloudwatchLogPlugin struct {
-	region string
+	region    string
+	groupName string
 }
 
 func (s cloudwatchLogPlugin) GetTaskLog(podName, namespace, containerName, containerId, logName string) (core.TaskLog, error) {
+
+	// Container IDs are prefixed with docker://, cri-o://, etc. which is stripped by fluentd before pushing to a log
+	// stream.  Therefore, we must also strip the prefix.
+	// Also, container names are
+	stripDelimiter := "://"
+	if split := strings.Split(containerId, stripDelimiter); len(split) > 1 {
+		containerId = split[1]
+	}
+
 	return core.TaskLog{
 		Uri: fmt.Sprintf(
-			"https://console.aws.amazon.com/cloudwatch/home?region=%s#logEventViewer:group=/flyte/kubernetes;stream=var.log.containers.%s_%s_%s-%s.log",
+			"https://console.aws.amazon.com/cloudwatch/home?region=%s#logEventViewer:group=%s;stream=var.log.containers.%s_%s_%s-%s.log",
 			s.region,
+			s.groupName,
 			podName,
 			namespace,
 			containerName,
@@ -23,8 +35,9 @@ func (s cloudwatchLogPlugin) GetTaskLog(podName, namespace, containerName, conta
 	}, nil
 }
 
-func NewCloudwatchLogPlugin(region string) LogPlugin {
+func NewCloudwatchLogPlugin(region, groupName string) LogPlugin {
 	return &cloudwatchLogPlugin{
-		region: region,
+		region:    region,
+		groupName: groupName,
 	}
 }
--- clients/go/coreutils/logs/cloudwatch_test.go
+++ clients/go/coreutils/logs/cloudwatch_test.go
@@ -6,8 +6,36 @@ import (
 	"testing"
 )
 
-func TestCloudwatchLogMaker(t *testing.T) {
-	p := NewCloudwatchLogPlugin("us-east-1")
+func TestCloudwatchLogMaker_CriO(t *testing.T) {
+	p := NewCloudwatchLogPlugin("us-east-1", "/flyte-production/kubernetes")
+	tl, err := p.GetTaskLog(
+		"f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver",
+		"modelbuildertemplate-staging",
+		"spark-kubernetes-driver",
+		"cri-o://aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae",
+		"main_logs")
+	assert.NoError(t, err)
+	assert.Equal(t, tl.GetName(), "main_logs")
+	assert.Equal(t, tl.GetMessageFormat(), core.TaskLog_JSON)
+	assert.Equal(t, tl.Uri, "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/flyte-production/kubernetes;stream=var.log.containers.f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver_modelbuildertemplate-staging_spark-kubernetes-driver-aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae.log")
+}
+
+func TestCloudwatchLogMaker_Docker(t *testing.T) {
+	p := NewCloudwatchLogPlugin("us-east-1", "/flyte-staging/kubernetes")
+	tl, err := p.GetTaskLog(
+		"f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver",
+		"modelbuildertemplate-staging",
+		"spark-kubernetes-driver",
+		"docker://aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae",
+		"main_logs")
+	assert.NoError(t, err)
+	assert.Equal(t, tl.GetName(), "main_logs")
+	assert.Equal(t, tl.GetMessageFormat(), core.TaskLog_JSON)
+	assert.Equal(t, tl.Uri, "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/flyte-staging/kubernetes;stream=var.log.containers.f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver_modelbuildertemplate-staging_spark-kubernetes-driver-aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae.log")
+}
+
+func TestCloudwatchLogMaker_NoPrefix(t *testing.T) {
+	p := NewCloudwatchLogPlugin("us-east-1", "/flyte-staging/kubernetes")
 	tl, err := p.GetTaskLog(
 		"f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver",
 		"modelbuildertemplate-staging",
@@ -17,5 +45,5 @@ func TestCloudwatchLogMaker(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, tl.GetName(), "main_logs")
 	assert.Equal(t, tl.GetMessageFormat(), core.TaskLog_JSON)
-	assert.Equal(t, tl.Uri, "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/flyte/kubernetes;stream=var.log.containers.f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver_modelbuildertemplate-staging_spark-kubernetes-driver-aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae.log")
+	assert.Equal(t, tl.Uri, "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/flyte-staging/kubernetes;stream=var.log.containers.f-f66f663c-2b36-4dfe-8a5f-884cbe61e596-773460-driver_modelbuildertemplate-staging_spark-kubernetes-driver-aac2ca7493fb7b1784ab64bb38b08b8777c740935834da36d20b242e60a76dae.log")
 }
