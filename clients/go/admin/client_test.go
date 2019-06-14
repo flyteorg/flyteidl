ae6f56fd7082b573c5976d31efcf4c4ecc86a98c
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-06-14 16:51:15 -0700

    Closure type and node

--- clients/go/admin/client_test.go
+++ clients/go/admin/client_test.go
@@ -2,17 +2,22 @@ package admin
 
 import (
 	"context"
+	"crypto/x509"
 	"net/url"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/lyft/flytestdlib/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	"github.com/lyft/flytestdlib/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestInitializeAndGetAdminClient(t *testing.T) {
-
 	ctx := context.TODO()
 	t.Run("legal", func(t *testing.T) {
 		u, err := url.Parse("http://localhost:8089")
@@ -27,6 +32,34 @@ func TestInitializeAndGetAdminClient(t *testing.T) {
 		once = sync.Once{}
 		assert.NotNil(t, InitializeAdminClient(ctx, Config{}))
 	})
+
+	t.Run("real", func(t *testing.T) {
+		u, err := url.Parse("dns:flyte-staging-internal.lyft.net")
+		assert.NoError(t, err)
+
+		c := InitializeAdminClient(ctx, Config{
+			Endpoint: config.URL{URL: *u},
+		})
+		assert.NotNil(t, c)
+
+		var opts []grpc.DialOption
+		pool, err := x509.SystemCertPool()
+		assert.NoError(t, err)
+		creds := credentials.NewClientTLSFromCert(pool, "")
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+
+		adminConnection, err = grpc.DialContext(ctx, "dns:///flyte-staging-internal.lyft.net", opts...)
+		if err != nil {
+			logger.Panicf(ctx, "failed to initialize Admin connection. Err: %s", err.Error())
+		}
+
+		c = InitializeAdminClient(ctx, Config{
+			Endpoint: config.URL{URL: *u},
+		})
+		assert.NotNil(t, c)
+		_, err = c.ListProjects(ctx, &admin.ProjectListRequest{})
+		assert.NoError(t, err)
+	})
 }
 
 func TestInitializeMockAdminClient(t *testing.T) {
@@ -37,7 +70,7 @@ func TestInitializeMockAdminClient(t *testing.T) {
 func TestGetAdditionalAdminClientConfigOptions(t *testing.T) {
 	u, _ := url.Parse("localhost:8089")
 	adminServiceConfig := Config{
-		Endpoint:              config.URL{URL: *u,},
+		Endpoint:              config.URL{URL: *u},
 		UseInsecureConnection: true,
 		PerRetryTimeout:       config.Duration{1 * time.Second},
 		MaxRetries:            1,
--- clients/go/events/config_flags.go
+++ clients/go/events/config_flags.go
@@ -6,13 +6,30 @@ package events
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/spf13/pflag"
 )
 
+// If v is a pointer, it will get its element value or the zero value of the element type.
+// If v is not a pointer, it will return it as is.
+func (Config) elemValueOrNil(v interface{}) interface{} {
+	if t := reflect.TypeOf(v); t.Kind() == reflect.Ptr {
+		if reflect.ValueOf(v).IsNil() {
+			return reflect.Zero(t.Elem()).Interface()
+		} else {
+			return reflect.ValueOf(v).Interface()
+		}
+	} else if v == nil {
+		return reflect.Zero(t).Interface()
+	}
+
+	return v
+}
+
 // GetPFlagSet will return strongly types pflags for all fields in Config and its nested types. The format of the
 // flags is json-name.json-sub-name... etc.
-func (Config) GetPFlagSet(prefix string) *pflag.FlagSet {
+func (cfg Config) GetPFlagSet(prefix string) *pflag.FlagSet {
 	cmdFlags := pflag.NewFlagSet("Config", pflag.ExitOnError)
 	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "type"), *new(string), "Sets the type of EventSink to configure [log/admin/file].")
 	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "file-path"), *new(string), "For file types,  specify where the file should be located.")
