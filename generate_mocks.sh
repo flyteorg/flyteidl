01440bbaeb73105e1b99a3819f430544963a27b9
Hongxin Liang <honnix@spotify.com>
2019-12-18 00:10:47 +0100

    install mockery in a temp folder

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 set -x
-which mockery || (go get github.com/vektra/mockery/cmd/mockery)
+
+go_get_mockery () {
+  tmp_dir=$(mktemp -d -t mockery-XXXXXXXXXX)
+  cp go.mod go.sum "$tmp_dir"
+  cd "$tmp_dir"
+  go get github.com/vektra/mockery/cmd/mockery
+  cd -
+}
+
+which mockery || go_get_mockery
 
 mockery -dir=gen/pb-go/flyteidl/service/ -name=AdminServiceClient -output=clients/go/admin/mocks
 mockery -dir=gen/pb-go/flyteidl/datacatalog/ -name=ArtifactsClient -output=clients/go/datacatalog/mocks
-
-# workaround https://github.com/golang/go/issues/33008
-# here we can not install outside of current module because we need the replacement in
-# go.mod
-rm go.sum
-go mod tidy
