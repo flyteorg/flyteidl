6e00aab51b81f2614721f48acc1990aad054c4b8
Hongxin Liang <honnix@spotify.com>
2020-01-21 10:45:29 +0100

    rely on Makefile to install mockery

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -2,17 +2,5 @@
 set -e
 set -x
 
-# install outside of current module, workaround https://github.com/golang/go/issues/30515
-# we need to copy go.mod and go.sum because we have a mockery fork
-go_get_mockery () {
-  tmp_dir=$(mktemp -d -t mockery-XXXXXXXXXX)
-  cp go.mod go.sum "$tmp_dir"
-  cd "$tmp_dir"
-  go get github.com/vektra/mockery/cmd/mockery
-  cd -
-}
-
-which mockery || go_get_mockery
-
 mockery -dir=gen/pb-go/flyteidl/service/ -name=AdminServiceClient -output=clients/go/admin/mocks
 mockery -dir=gen/pb-go/flyteidl/datacatalog/ -name=ArtifactsClient -output=clients/go/datacatalog/mocks
