98d8f189c9d6c53af6c6b76fce61180bd9d622e1
Katrina Rogan <krogan@lyft.com>
2020-02-10 16:03:25 -0800

    wip

--- clients/go/admin/mocks/AdminServiceClient.go
+++ clients/go/admin/mocks/AdminServiceClient.go
@@ -1265,6 +1265,54 @@ func (_m *AdminServiceClient) ListLaunchPlans(ctx context.Context, in *admin.Res
 	return r0, r1
 }
 
+type AdminServiceClient_ListMatchableResources struct {
+	*mock.Call
+}
+
+func (_m AdminServiceClient_ListMatchableResources) Return(_a0 *admin.ListMatchableResourcesResponse, _a1 error) *AdminServiceClient_ListMatchableResources {
+	return &AdminServiceClient_ListMatchableResources{Call: _m.Call.Return(_a0, _a1)}
+}
+
+func (_m *AdminServiceClient) OnListMatchableResources(ctx context.Context, in *admin.ListMatchableResourcesRequest, opts ...grpc.CallOption) *AdminServiceClient_ListMatchableResources {
+	c := _m.On("ListMatchableResources", ctx, in, opts)
+	return &AdminServiceClient_ListMatchableResources{Call: c}
+}
+
+func (_m *AdminServiceClient) OnListMatchableResourcesMatch(matchers ...interface{}) *AdminServiceClient_ListMatchableResources {
+	c := _m.On("ListMatchableResources", matchers...)
+	return &AdminServiceClient_ListMatchableResources{Call: c}
+}
+
+// ListMatchableResources provides a mock function with given fields: ctx, in, opts
+func (_m *AdminServiceClient) ListMatchableResources(ctx context.Context, in *admin.ListMatchableResourcesRequest, opts ...grpc.CallOption) (*admin.ListMatchableResourcesResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *admin.ListMatchableResourcesResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *admin.ListMatchableResourcesRequest, ...grpc.CallOption) *admin.ListMatchableResourcesResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*admin.ListMatchableResourcesResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *admin.ListMatchableResourcesRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 type AdminServiceClient_ListNamedEntities struct {
 	*mock.Call
 }
