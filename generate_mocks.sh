62ead112462f58479ec5f257a0a328bb3fa9a3f5
pmahindrakar-oss <77798312+pmahindrakar-oss@users.noreply.github.com>
2021-04-14 22:47:28 +0530

    Added Initializing auth client from config method (#140)
    * Added Initializing auth client from config method
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Using sync.Once only once for initializing all the clients
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Added mock generation for AuthServiceClient
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Added clientset wrapping adminClient and authClient
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Added initialization of the mock client set
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Exposed the AdminServiceClient and AuthServiceClient for mocking
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Incorporated the feedback
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    Co-authored-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -3,4 +3,5 @@ set -e
 set -x
 
 mockery -dir=gen/pb-go/flyteidl/service/ -name=AdminServiceClient -output=clients/go/admin/mocks
+mockery -dir=gen/pb-go/flyteidl/service/ -name=AuthServiceClient -output=clients/go/admin/mocks
 mockery -dir=gen/pb-go/flyteidl/datacatalog/ -name=DataCatalogClient -output=clients/go/datacatalog/mocks
