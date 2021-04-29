05e36c61708a4a25628a16634b231a83ebe8c002
Haytham Abuelfutuh <haytham@afutuh.com>
2021-04-29 11:21:39 -0700

    event init error
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- clients/go/events/admin_eventsink.go
+++ clients/go/events/admin_eventsink.go
@@ -84,7 +84,7 @@ func (s *adminEventSink) Close() error {
 
 func initializeAdminClientFromConfig(ctx context.Context) (client service.AdminServiceClient, err error) {
 	cfg := admin2.GetConfig(ctx)
-	clients, err := admin2.initializeClients(ctx, cfg, nil)
+	clients, err := admin2.NewClientsetBuilder().WithConfig(cfg).Build(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize clientset. Error: %w", err)
 	}
