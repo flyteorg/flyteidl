c4e27e8c9c78b0d758f505e30fb2e7cf59abc05e
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-12-20 19:25:35 +0530

    Added flag to pass in CAcerts
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- clients/go/admin/config_flags_test.go
+++ clients/go/admin/config_flags_test.go
@@ -141,6 +141,20 @@ func TestConfig_SetFlags(t *testing.T) {
 			}
 		})
 	})
+	t.Run("Test_caCerts", func(t *testing.T) {
+
+		t.Run("Override", func(t *testing.T) {
+			testValue := "1"
+
+			cmdFlags.Set("caCerts", testValue)
+			if vString, err := cmdFlags.GetString("caCerts"); err == nil {
+				testDecodeJson_Config(t, fmt.Sprintf("%v", vString), &actual.CAcerts)
+
+			} else {
+				assert.FailNow(t, err.Error())
+			}
+		})
+	})
 	t.Run("Test_maxBackoffDelay", func(t *testing.T) {
 
 		t.Run("Override", func(t *testing.T) {
