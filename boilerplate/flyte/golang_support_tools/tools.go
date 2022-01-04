d3534d7812db30901df76a499a2361fa33f69dc0
Katrina Rogan <katroganGH@gmail.com>
2022-01-04 14:14:09 -0800

    revert
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- boilerplate/flyte/golang_support_tools/tools.go
+++ boilerplate/flyte/golang_support_tools/tools.go
@@ -7,5 +7,5 @@ import (
 	_ "github.com/flyteorg/flytestdlib/cli/pflags"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/vektra/mockery/cmd/mockery"
-	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+	- "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
 )
