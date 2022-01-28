78df963d64ee5e8f9df777e508a46ed504831015
Sean Lin <sean@union.ai>
2022-01-27 17:44:42 -0800

    Expose flyteadmin client's auth opt Signed-off-by: Sean Lin <sean@union.ai>

--- clients/go/admin/client.go
+++ clients/go/admin/client.go
@@ -41,6 +41,11 @@ type Clientset struct {
 	authMetadataServiceClient service.AuthMetadataServiceClient
 	healthServiceClient       grpc_health_v1.HealthClient
 	identityServiceClient     service.IdentityServiceClient
+	authOpt                   grpc.DialOption
+}
+
+func (c Clientset) AuthOpt() grpc.DialOption {
+	return c.authOpt
 }
 
 // AdminClient retrieves the AdminServiceClient
@@ -177,6 +182,7 @@ func InitializeAdminClient(ctx context.Context, cfg *Config, opts ...grpc.DialOp
 // initializeClients creates an AdminClient, AuthServiceClient and IdentityServiceClient with a shared Admin connection
 // for the process. Note that if called with different cfg/dialoptions, it will not refresh the connection.
 func initializeClients(ctx context.Context, cfg *Config, tokenCache pkce.TokenCache, opts ...grpc.DialOption) (*Clientset, error) {
+	var cs Clientset
 	once.Do(func() {
 		authMetadataClient, err := InitializeAuthMetadataClient(ctx, cfg)
 		if err != nil {
@@ -194,6 +200,7 @@ func initializeClients(ctx context.Context, cfg *Config, tokenCache pkce.TokenCa
 		}
 
 		if opt != nil {
+			cs.authOpt = opt
 			opts = append(opts, opt)
 		}
 
@@ -203,7 +210,6 @@ func initializeClients(ctx context.Context, cfg *Config, tokenCache pkce.TokenCa
 		}
 	})
 
-	var cs Clientset
 	cs.adminServiceClient = NewAdminClient(ctx, adminConnection)
 	cs.authMetadataServiceClient = service.NewAuthMetadataServiceClient(adminConnection)
 	cs.identityServiceClient = service.NewIdentityServiceClient(adminConnection)
