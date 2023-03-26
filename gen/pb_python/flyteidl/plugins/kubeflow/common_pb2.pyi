058cafdf497bed6fd8d78bac8c50a49402940a1c
Yubo Wang <yubwang@linkedin.com>
2023-03-25 17:12:57 -0700

    refactor kubeflow operators proto
    Signed-off-by: Yubo Wang <yubwang@linkedin.com>

---
+++ gen/pb_python/flyteidl/plugins/kubeflow/common_pb2.pyi
@@ -0,0 +1,36 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+CLEANPOD_POLICY_ALL: CleanPodPolicy
+CLEANPOD_POLICY_NONE: CleanPodPolicy
+CLEANPOD_POLICY_RUNNING: CleanPodPolicy
+CLEANPOD_POLICY_UNDEFINED: CleanPodPolicy
+DESCRIPTOR: _descriptor.FileDescriptor
+RESTART_POLICY_ALWAYS: RestartPolicy
+RESTART_POLICY_NEVER: RestartPolicy
+RESTART_POLICY_ON_FAILURE: RestartPolicy
+SUCCESS_POLICY_ALL_WORKERS: SuccessPolicy
+SUCCESS_POLICY_DEFAULT: SuccessPolicy
+
+class RunPolicy(_message.Message):
+    __slots__ = ["activeDeadlineSeconds", "backoff_limit", "clean_pod_policy", "ttl_seconds_after_finished"]
+    ACTIVEDEADLINESECONDS_FIELD_NUMBER: _ClassVar[int]
+    BACKOFF_LIMIT_FIELD_NUMBER: _ClassVar[int]
+    CLEAN_POD_POLICY_FIELD_NUMBER: _ClassVar[int]
+    TTL_SECONDS_AFTER_FINISHED_FIELD_NUMBER: _ClassVar[int]
+    activeDeadlineSeconds: int
+    backoff_limit: int
+    clean_pod_policy: CleanPodPolicy
+    ttl_seconds_after_finished: int
+    def __init__(self, clean_pod_policy: _Optional[_Union[CleanPodPolicy, str]] = ..., ttl_seconds_after_finished: _Optional[int] = ..., activeDeadlineSeconds: _Optional[int] = ..., backoff_limit: _Optional[int] = ...) -> None: ...
+
+class SuccessPolicy(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
+
+class CleanPodPolicy(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
+
+class RestartPolicy(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
---
+++ gen/pb_python/flyteidl/plugins/kubeflow/tensorflow_pb2.pyi
@@ -0,0 +1,30 @@
+from flyteidl.plugins.kubeflow import common_pb2 as _common_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class DistributedTensorflowTrainingReplicaSpec(_message.Message):
+    __slots__ = ["pod_template_name", "replicas", "restart_policy"]
+    POD_TEMPLATE_NAME_FIELD_NUMBER: _ClassVar[int]
+    REPLICAS_FIELD_NUMBER: _ClassVar[int]
+    RESTART_POLICY_FIELD_NUMBER: _ClassVar[int]
+    pod_template_name: str
+    replicas: int
+    restart_policy: _common_pb2.RestartPolicy
+    def __init__(self, replicas: _Optional[int] = ..., pod_template_name: _Optional[str] = ..., restart_policy: _Optional[_Union[_common_pb2.RestartPolicy, str]] = ...) -> None: ...
+
+class DistributedTensorflowTrainingTask(_message.Message):
+    __slots__ = ["chief_replicas", "ps_replicas", "run_policy", "success_policy", "worker_replicas"]
+    CHIEF_REPLICAS_FIELD_NUMBER: _ClassVar[int]
+    PS_REPLICAS_FIELD_NUMBER: _ClassVar[int]
+    RUN_POLICY_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_POLICY_FIELD_NUMBER: _ClassVar[int]
+    WORKER_REPLICAS_FIELD_NUMBER: _ClassVar[int]
+    chief_replicas: DistributedTensorflowTrainingReplicaSpec
+    ps_replicas: DistributedTensorflowTrainingReplicaSpec
+    run_policy: _common_pb2.RunPolicy
+    success_policy: _common_pb2.SuccessPolicy
+    worker_replicas: DistributedTensorflowTrainingReplicaSpec
+    def __init__(self, worker_replicas: _Optional[_Union[DistributedTensorflowTrainingReplicaSpec, _Mapping]] = ..., ps_replicas: _Optional[_Union[DistributedTensorflowTrainingReplicaSpec, _Mapping]] = ..., chief_replicas: _Optional[_Union[DistributedTensorflowTrainingReplicaSpec, _Mapping]] = ..., run_policy: _Optional[_Union[_common_pb2.RunPolicy, _Mapping]] = ..., success_policy: _Optional[_Union[_common_pb2.SuccessPolicy, str]] = ...) -> None: ...
